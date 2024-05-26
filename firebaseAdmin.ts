ce776906d0049ad93811157a7f85e9693c1b9c71
phaneendra97 <phaneendra1997@gmail.com>
2024-05-25 20:05:35 -0700

    reconfig firebase

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin';
-const serviceAccount = require('./sih-super-set-dev-firebase-adminsdk-ayuh1-1ee1709ed8.json'); 
+// const serviceAccount = require('./sih-super-set-dev-firebase-adminsdk-ayuh1-1ee1709ed8.json'); 
+const serviceAccount = require('/etc/secrets/sih-super-set-dev-firebase-adminsdk-ayuh1-1ee1709ed8.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
